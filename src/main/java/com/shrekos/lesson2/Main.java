d9ddce413606cdd66b70ecd826ac0a2ad70ef0f8
alex97po <sas.pogorelov@gmail.com>
2018-07-23 22:04:04 +0300

    initial commit

---
+++ src/main/java/com/shrekos/lesson2/Main.java
@@ -0,0 +1,12 @@
+package com.shrekos.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+      Calc calc = new Calc();
+        System.out.println(Calc.add(3,2));
+        System.out.println(Calc.odd(6,5));
+        System.out.println(Calc.mult(6,7));
+        System.out.println(Calc.div(32, 4));
+        System.out.println(Calc.sqrt(64));
+    }
+}
