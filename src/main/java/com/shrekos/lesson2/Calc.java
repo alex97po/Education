8a0e266e5235345d6d6d599e214557ba90eefc35
alex97po <sas.pogorelov@gmail.com>
2018-07-25 21:22:50 +0300

    add not default construction

--- src/main/java/com/shrekos/lesson2/Calc.java
+++ src/main/java/com/shrekos/lesson2/Calc.java
@@ -1,6 +1,17 @@
 package com.shrekos.lesson2;
 
 public class Calc {
+    public int chargeLevel;
+    public int accurancy;
+
+    public Calc() {
+    }
+
+    public Calc(int chargeLevel, int accurancy) {
+        this.chargeLevel = chargeLevel;
+        this.accurancy = accurancy;
+    }
+
     public static int add(int a, int b) {
         return a+b;
     }
