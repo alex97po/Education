6368bab06814cb6173d251d12e731de83cb389d5
alex97po <sas.pogorelov@gmail.com>
2018-07-29 10:58:43 +0300

    Added P calculations

--- src/main/java/com/shrekos/lesson3/Triangle.java
+++ src/main/java/com/shrekos/lesson3/Triangle.java
@@ -6,10 +6,13 @@ public class Triangle {
    public static Points point3 = new Points (-6, 5);
 
    public static double S = ((point1.x - point3.x)*(point2.y - point3.y)-(point2.x - point3.x)*(point1.y - point3.y))/2;
+   public static double Side12 = Math.sqrt(Math.pow((point1.x - point2.x),2) + Math.pow((point1.y - point2.y),2));
+   public static double Side23 = Math.sqrt(Math.pow((point2.x - point3.x),2) + Math.pow((point2.y - point3.y),2));
+   public static double Side13 = Math.sqrt(Math.pow((point1.x - point3.x),2) + Math.pow((point1.y - point3.y),2));
+   public static double P = Side12 + Side23 + Side13;
 
     public static void main(String[] args) {
-        System.out.println(S);
+        System.out.println("S = "+S);
+        System.out.println("P = "+P);
     }
-
-    //null
 }
