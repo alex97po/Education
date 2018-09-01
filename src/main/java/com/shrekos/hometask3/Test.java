5a681112f4ce4aded9e18bd89d23fdf9fba197d6
alex97po <sas.pogorelov@gmail.com>
2018-09-01 15:39:31 +0300

    bags for case with arr.length == 1 and case with right order

--- src/main/java/com/shrekos/hometask3/Test.java
+++ src/main/java/com/shrekos/hometask3/Test.java
@@ -8,10 +8,12 @@ public class Test {
             inputNumbers [y] = Integer.parseInt(numbersLineArr[y]);
         }
         boolean order = true;
-        for (int i = 0; i < inputNumbers.length; i++) {
-            if (inputNumbers[i] > inputNumbers [i+1]) {
-                order = false;
-                break;
+        if (inputNumbers.length != 1) {
+            for (int i = 0; i < inputNumbers.length - 1; i++) {
+                if (inputNumbers[i] > inputNumbers[i + 1]) {
+                    order = false;
+                    break;
+                }
             }
         }
             return  order;
