92ab1df48fa1394fd0a8953b249b99f67b387fa5
alex97po <sas.pogorelov@gmail.com>
2018-09-01 15:23:54 +0300

    initial commit, fix case where input number == 1 required

--- src/main/java/com/shrekos/hometask3/Main.java
+++ src/main/java/com/shrekos/hometask3/Main.java
@@ -1,7 +1,12 @@
 package com.shrekos.hometask3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Print numbers with comma: ");
+        boolean testResult = Test.test(in.nextLine());
+        System.out.println("Asc order: " + testResult);
     }
 }
--- src/main/java/com/shrekos/hometask3/Test.java
+++ src/main/java/com/shrekos/hometask3/Test.java
@@ -1,4 +1,19 @@
 package com.shrekos.hometask3;
 
 public class Test {
+    public static boolean test (String input) {
+        String numbersLineArr [] = input.split (",");
+        int inputNumbers [] = new int[numbersLineArr.length];
+        for (int y = 0; y < inputNumbers.length; y++) {
+            inputNumbers [y] = Integer.parseInt(numbersLineArr[y]);
+        }
+        boolean order = true;
+        for (int i = 0; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] > inputNumbers [i+1]) {
+                order = false;
+                break;
+            }
+        }
+            return  order;
+    }
 }
