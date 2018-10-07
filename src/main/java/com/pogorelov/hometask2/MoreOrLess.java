43b8456dc36eab6909ee7f91543fc3ee3e2b6a20
alex97po <sas.pogorelov@gmail.com>
2018-10-07 16:10:53 +0300

    Input override fixed

--- src/main/java/com/pogorelov/hometask2/MoreOrLess.java
+++ src/main/java/com/pogorelov/hometask2/MoreOrLess.java
@@ -1,25 +1,27 @@
 package com.pogorelov.hometask2;
 
 public class MoreOrLess {
-    public static void moreless (int input) {
-        int target = (int) (Math.random()*101);
+    public static void moreless(int input) {
+        int target = (int) (Math.random() * 101);
         int rangeMin = 0;
         int rangeMax = 100;
         boolean flag = true;
         while (flag) {
             if (input == target) {
                 flag = false;
+                System.out.println("Congratulations! You are right, the number we were thinking of was " + target);
             } else if (input < target) {
                 rangeMin = input;
                 System.out.println("More");
-                System.out.println("["+input+"...x..."+rangeMax+"]");
+                System.out.println("[" + input + "...x..." + rangeMax + "]");
+                input = Viewer.viewer();
             } else {
                 rangeMax = input;
                 System.out.println("Less");
-                System.out.println("["+rangeMin+"...x..."+input+"]");
+                System.out.println("[" + rangeMin + "...x..." + input + "]");
+                input = Viewer.viewer();
             }
-            Viewer.viewer();
+
         }
-        System.out.println("Congratulations! You are rught, the number we were thinking of was "+target);
     }
 }
--- src/main/java/com/pogorelov/hometask2/Viewer.java
+++ src/main/java/com/pogorelov/hometask2/Viewer.java
@@ -3,7 +3,7 @@ package com.pogorelov.hometask2;
 import java.util.Scanner;
 
 public class Viewer {
-    public static int viewer () {
+    public static int viewer() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Try to guess a number: ");
         int input = scanner.nextInt();
